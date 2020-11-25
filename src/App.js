4489a1e92bed27abb662fe71f1939fb4c0eb367d
indranil2507 <iindranilsaha1@gmail.com>
2020-11-25 09:15:34 +0530

    products

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import './App.css';
 import {BrowserRouter as Router} from 'react-router-dom';
 import { GlobalStyle } from './globalStyles';
 import Hero from './components/Home';
+import { productData } from './components/Products/data';
+import Products from './components/Products';
 function App() {
   return (
     <>
     <Router >
       <GlobalStyle/>
       <Hero/>
+      <Products heading='Choose your Favourite' data={productData}/>
       
       </Router>
  
